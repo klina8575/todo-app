339bf0c37aa93eb6dbf94008218f8413da7f3280
klina8575 <klina8575@gmail.com>
2022-04-26 18:59:58 +0900

    init

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,17 @@
+// 일정 추가
+import React from "react";
+import { MdAdd } from "react-icons/md";
+import "./TodoInsert.scss";
+
+function TodoInsert(props) {
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+}
+
+export default TodoInsert;
