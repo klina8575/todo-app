e19d64a92409617c76e58370b160e60557ce0e23
klina8575 <klina8575@gmail.com>
2022-04-27 15:46:32 +0900

    up'

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,16 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 
-function TodoList({ todos, onRemove }) {
+function TodoList({ todos, onRemove, onToggle }) {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
